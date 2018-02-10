9dec2123c8c6629bd6d2957e6b31cecc69fd1c60
MrGrimace <alexandre.anchyse@gmail.com>
2018-02-10 17:11:32 +0100

    Enemy AI

---
+++ 2D Platformer/Assets/EnnemyAI.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
+
+public class EnnemyAI : MonoBehaviour {
+
+    public Transform target;
+
+    //How many times each second we will update our path
+    public float updateRate = 2f;
+
+    //Caching
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    //The calculated path
+    public Path path;
+
+    //The AI's speed per second 
+    public float speed = 300f;
+    public ForceMode2D fMode;
+
+    [HideInInspector]
+    public bool pathisEnded = false;
+
+    //The max distance from the AI to a waypoint for it to continue to the next waypoint
+    public float nextWaypointDistance = 3;
+
+    private int currentWayPoint = 0;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        if(target==null)
+        {
+            Debug.LogError("No player found");
+        }
+        //Start a new path to the target position, return the result to the OnPathComplete method
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        StartCoroutine(UpdatePath());
+    }
+
+    IEnumerator UpdatePath()
+    {
+        if(target==null)
+        {
+            //TODO Insert a player search here
+            yield break;
+        }
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        yield return new WaitForSeconds(1f / updateRate);
+        StartCoroutine(UpdatePath());
+
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("We got a path. Did it have an error?" + p.error);
+        if(!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (target == null)
+        {
+            //TODO Insert a player search here
+            return;
+        }
+
+        //TODO Always look at player?
+
+        if(path==null)
+        {
+            return;
+        }
+        
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            if (pathisEnded)
+                return;
+            Debug.Log("End of path reached");
+            pathisEnded = true;
+            return;
+        }
+
+        pathisEnded = false;
+
+        //Direction to the next Waypoint
+        Vector3 dir = (path.vectorPath[currentWayPoint] - transform.position).normalized;
+        dir *= speed * Time.fixedDeltaTime;
+
+        //move the AI
+        rb.AddForce(dir,fMode);
+
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWayPoint]);
+
+        if (dist<nextWaypointDistance)
+        {
+            currentWayPoint++;
+            return;
+        }
+    }
+}
---
+++ 2D Platformer/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster gm;
+
+    private void Start()
+    {
+        if(gm==null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public float spawnDelay = 5;
+    public Transform spawnPrefab;
+
+    public IEnumerator RespawnPlayer()
+    {
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Transform clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation);
+        Destroy(clone.gameObject, 3f);
+        
+    }
+
+    public static void KillPlayer (Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+    }
+
+    
+}
